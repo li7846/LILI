b08d4ca56abe69cb7de0510a839d0d2e27869332
li7846 <1374680016@qq.com>
2016-01-18 16:22:40 +0800

    diyige

---
+++ dasdad/dasdad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dasdad
+//
+//  Created by qianfeng on 16/1/18.
+//  Copyright (c) 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
